4bd759a2f6615e15bb4fa92cdc0fb6aceb1dd815
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-01-25 23:51:06 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,18 @@
+package loop;
+
+import java.util.*;
+
+public class reverse {
+    public static void main(String[] args) {
+        Scanner r=new Scanner(System.in);
+        int n=r.nextInt();
+        int rev=0;
+        while(n>0) {
+            int lastdigit=n%10;
+            rev=(rev*10)+lastdigit;
+            n=n/10;
+        }
+        System.out.println(rev);
+        r.close();
+    }
+}
