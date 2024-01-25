1cbe3cade7f3dccbe761472c6130968624dcf1d7
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-01-26 00:25:11 +0530

    Create temperature.java

---
+++ temperature.java
@@ -0,0 +1,13 @@
+package PS3;
+
+public class temperature {
+    public static void main(String[] args) {
+        double temp=103.5;
+        if(temp>100) {
+            System.out.println("you have a fever");
+        }
+        else {
+            System.out.println("you don't have a fever");
+        }
+    }
+}
